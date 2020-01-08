1679984291327dd2945e70c3585d62d0d4265ab5
micahfitz14 <47066969+micahfitz14@users.noreply.github.com>
2020-01-08 11:18:06 -0800

    created lab 1

---
+++ CPE233 Labs/CPE233 Labs.srcs/sources_1/new/lab1_fibonacci_seq_gen.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/08/2020 11:17:32 AM
+// Design Name: 
+// Module Name: fibonacci_seq_gen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fibonacci_seq_gen(
+    input BTN,
+    output [3:0] disp_en,
+    output [7:0] ssegs,
+    input clk
+    );
+endmodule
