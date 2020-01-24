1d3f1c8710b4f43c78452095ed5c39f3d76c164e
micahfitz14 <47066969+micahfitz14@users.noreply.github.com>
2020-01-24 13:31:42 -0800

    lab3 prep

--- CPE233 Labs/project_1/project_1.srcs/sources_1/new/ALU.v
+++ CPE233 Labs/project_1/project_1.srcs/sources_1/new/ALU.v
@@ -11,6 +11,29 @@
 
 
 module alu(
-
+        input [31:0] OP_1, 
+        input [31:0] OP_2,
+        input [3:0] alu_fun,
+        output reg [31:0] RESULT  
     );
+    
+    always@(alu_fun)
+    begin
+        case(alu_fun)
+        
+            4'b0000 : RESULT = OP_1 + OP_2;
+            4'b0001 : RESULT = OP_1 << OP_2; //shift OP_1 left by the value of OP_2
+            4'b0010 : if($signed(OP_1) < $signed(OP_2))
+                            RESULT = 1;
+                      else
+                            RESULT = 0;
+            4'b0011 : if(OP_1 < OP_2)
+                            RESULT = 1;
+                      else
+                            RESULT = 0;
+            4'b0100 :                 
+            
+            default : RESULT = 0;
+       endcase 
+    end
 endmodule
