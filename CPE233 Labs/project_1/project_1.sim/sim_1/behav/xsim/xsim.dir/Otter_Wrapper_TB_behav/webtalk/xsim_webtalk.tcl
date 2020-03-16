a13abd26fc36a24765f078d264e5af95de8b0ffc
Micah <47066969+micahfitz14@users.noreply.github.com>
2020-03-16 15:42:30 -0700

    edits

--- CPE233 Labs/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/Otter_Wrapper_TB_behav/webtalk/xsim_webtalk.tcl
+++ CPE233 Labs/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/Otter_Wrapper_TB_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/micah/Documents/GitHub/CPE233/CPE233 Labs/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/Otter_Wrapper_TB_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Mar 16 13:45:22 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Mar 16 15:38:19 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "34e2a3a1-5dc9-4055-aea5-d89143841985" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "e85d564cdc5b424cabd566c9c949d7e1" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "52" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "58" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-8550U CPU @ 1.80GHz" -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "16.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "51 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6200_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2072518922 -regid "" -xml C:/Users/micah/Documents/GitHub/CPE233/CPE233 Labs/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/Otter_Wrapper_TB_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/micah/Documents/GitHub/CPE233/CPE233 Labs/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/Otter_Wrapper_TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/micah/Documents/GitHub/CPE233/CPE233 Labs/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/Otter_Wrapper_TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6388_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 74922322 -regid "" -xml C:/Users/micah/Documents/GitHub/CPE233/CPE233 Labs/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/Otter_Wrapper_TB_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/micah/Documents/GitHub/CPE233/CPE233 Labs/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/Otter_Wrapper_TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/micah/Documents/GitHub/CPE233/CPE233 Labs/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/Otter_Wrapper_TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
